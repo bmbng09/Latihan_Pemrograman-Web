7a25adbf30c191e2fce0ddcc26ff3dc8dd25404a
Bambang Istijab <bambangistijab30@gmail.com>
2024-12-04 11:49:11 +0700

    Add files via upload

---
+++ DataDiri.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YahQUIZZ</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div align="center">
+        <h2>YahQUIZZZ..</h2>
+        <div class="form-container">
+            <form action="mulai.php" method="POST">
+                    <input type="text" name="nama" id="nama" placeholder="Nama">
+                    <p id="error-Nama" class="peringatan"></p>
+
+                    <input type="text" name="nim" id="nim" placeholder="NIM">
+                    <p id="error-NIM" class="peringatan"></p>
+
+                    <button>Kirim</button>
+            </form>
+        </div>
+
+    <script src="script2.js"></script>
+</body>
+</html>
